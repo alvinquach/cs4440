8c63b4a0129a26adf1abeed17ab10a6ef2617277
Unknown <alvinquach91@gmail.com>
2017-03-05 00:38:12 -0800

    can store history

--- project1/shell.c
+++ project1/shell.c
@@ -3,14 +3,75 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include  <sys/types.h>
+#include <sys/types.h>
 
 #define MAX_LINE 255
+#define HISTORY_SIZE 10
+
+void add_history(char* history[], char* input, int* count) {
+
+    int i;
+
+    printf("Contents Before: \n");
+    for (i = 0; i < HISTORY_SIZE; i++) {
+        if (history[i] == NULL) break;
+        printf("\t%i %s\n", i, history[i]);
+    }
+    printf("\n");
+    printf("Count Before: %i\n", *count);
+
+    // Store the issued command into the history array, removing the first value and shifting the rest of the values over if necessary.
+    for (i = 0; i < HISTORY_SIZE; i++) {
+        
+        // If the history has already reached its limit, then copy the value from the next value.
+        if (*count >= HISTORY_SIZE) {
+
+            // If the index is not the last index, then copy the value from the next value.
+            if (i < HISTORY_SIZE - 1) {
+                strcpy(history[i], history[i + 1]);
+            }
+
+            // Copy the new command to the last index.
+            else {
+                strcpy(history[i], input);
+            }
+            
+        }
+        
+        // If the history has not reached the limit, then copy the new command to the first available index.
+        else if (!strcmp(history[i], "\0")) {
+            strcpy(history[i], input);
+            break;
+        }
+
+    }
+    
+    printf("Contents After: \n");
+    for (i = 0; i < HISTORY_SIZE; i++) {
+        if (history[i] == NULL) break;
+        printf("\t%i %s\n", i, history[i]);
+    }
+    printf("\n");
+
+    // Increment the history count.
+    *count += 1;
+
+}
 
 int main(void) {
 
     char input[MAX_LINE];
+
+    // Initialize command history array.
+    int history_count = 0;
+    char* history[HISTORY_SIZE];
+    int i;
+    for (i = 0; i < HISTORY_SIZE; i++) {
+        history[i] = malloc(MAX_LINE);
+        strcpy(history[i], "\0");
+    }
 
     while (1) {
         
@@ -31,6 +92,12 @@ int main(void) {
             printf("Exiting...\n");
             return 0;
         }
+
+        // TODO Detect "!!" and "!n" inputs.
+
+        // Store user input in history, even if its an invalid command.
+        add_history(history, input, &history_count);
+        printf("Count After: %i\n", history_count);
         
         // Fork a process.
         pid_t pid = fork();
@@ -63,13 +130,12 @@ int main(void) {
             char input_copy[strlen(input) - 1];
             strcpy(input_copy, input);
 
-
-
             // Get rid of the '&' at the end of the string.
             if (input_copy[strlen(input_copy) - 1] == '&') {
                 input_copy[strlen(input_copy) - 1] = 0;
             }
 
+            // Split input string by spaces.
             int count = 0;
             while(1) {
                 char *arg;
@@ -98,7 +164,7 @@ int main(void) {
         }
 
     }
+
     return 0;
+
 }
-
-
